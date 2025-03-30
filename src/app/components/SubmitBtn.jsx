6e7aa1e5b6ff11635b78f8a2d49b8f1b0ecf95e0
suhobokovv123 <rolland1403@gmail.com>
2025-03-30 16:29:27 +0200

    typo

--- src/app/components/SubmitBtn.jsx
+++ src/app/components/SubmitBtn.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function SubmitBtn() {
   return (
-    <button type="button" className="text-white bg-[linear-gradient(to_right,_var(--primary1)_16%,_var(--primary2)_70%)] bg-[length:150%] bg-right hover:bg-left active:bg-left focus:bg-left transition-all duration-750 ease-in-out focus:outline-none rounded-lg text-xl py-4 text-center cursor-pointer">Sand Message</button>
+    <button type="button" className="text-white bg-[linear-gradient(to_right,_var(--primary1)_16%,_var(--primary2)_70%)] bg-[length:150%] bg-right hover:bg-left active:bg-left focus:bg-left transition-all duration-750 ease-in-out focus:outline-none rounded-lg text-xl py-4 text-center cursor-pointer">Send Message</button>
   )
 }
